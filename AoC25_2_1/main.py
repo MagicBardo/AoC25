39398e214413fa38a224b45d20a750429bf1cce5
MagicBardo <MagicBardo@users.noreply.github.com>
2025-12-20 01:23:50 +0100

    first intention for task 2_1

--- AoC25_2_1/main.py
+++ AoC25_2_1/main.py
@@ -1,5 +1,30 @@
+def get_ranges(file_path:str) -> list:
+    with open(file_path) as f:
+        line:str = f.readline()
+        ranges = line.split(',')
+    return ranges
+
+def id_check(start:int, end:int) -> int:
+    ans = 0
+    for num in range(start, end+1):
+        num = str(num)
+        for seq_len in range(0, len(num)):
+            seq = num[seq_len]
+            if num.count(seq) == 2:
+                pass
+
+    return ans
+
 def main():
-    pass
+    file_path:str = "./AoC25_2_1/input.txt"
+    ranges:list = get_ranges(file_path)
+    for range in ranges:
+        start:int = int((range.split('-'))[0])
+        end:int = int((range.split('-'))[1])
+        solutions = id_check(start, end)
+
+    print(f"Answer: {ans}")
+
 
 if __name__ == "__main__":
     main()
